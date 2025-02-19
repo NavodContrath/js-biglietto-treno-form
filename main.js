7247dc90ce3a1dd2eef1e140bf516659f6d1f14c
NavodContrath <navod.contrath@gmail.com>
2025-02-19 16:33:09 +0100

    add milestone 3

--- main.js
+++ main.js
@@ -56,6 +56,12 @@ const formEl = document.querySelector("form")
 const fullNameEl = document.getElementById("fullname")
 const userKmEl = document.getElementById("userKm")
 const userAgeEl = document.getElementById("userAge")
+const ticketNameEl = document.getElementById("ticketName")
+const ticketAgeEl = document.getElementById("ticketAge")
+const ticketKmEl = document.getElementById("ticketKm")
+const ticketOfferEl = document.getElementById("ticketOffer")
+const ticketCarriageEl = document.getElementById("ticketCarriage")
+const ticketPriceEl = document.getElementById("ticketPrice")
 
 
 formEl.addEventListener('submit', function () {
@@ -63,20 +69,24 @@ formEl.addEventListener('submit', function () {
     let travelPrice = Number(userKmEl.value) * priceKm
     if (Number(userAgeEl.value) > 65) {
         const overDiscount = travelPrice * 40 / 100
-        console.log(travelPrice);
-        console.log(overDiscount);
         const discountOver = travelPrice - overDiscount
-        console.log(discountOver.toFixed(2));
+        let offer = "Over Discount"
+        ticketPriceEl.innerText = discountOver.toFixed(2)
+        ticketOfferEl.innerText = offer
     } else if (Number(userAgeEl.value) < 18) {
         const minorDiscount = travelPrice * 20 / 100
-        console.log(travelPrice);
-        console.log(minorDiscount);
         const discountMinor = travelPrice - minorDiscount
-        console.log(discountMinor.toFixed(2));
-    } else if (isNaN(userKmEl) || isNaN(userAgeEl)) {
-        alert("please insert a numeric value")
+        let offer = "Minor Discount"
+        ticketPriceEl.innerText = discountMinor.toFixed(2)
+        ticketOfferEl.innerText = offer
     } else {
-        console.log(travelPrice);
+        let offer = "Regular"
+        ticketPriceEl.innerText = travelPrice.toFixed(2)
+        ticketOfferEl.innerText = offer
     }
+    ticketNameEl.innerText = fullNameEl.value
+    ticketAgeEl.innerText = userAgeEl.value
+    ticketKmEl.innerText = userKmEl.value
+    ticketCarriageEl.innerText = Math.floor(Math.random() * 10 + 1)
 }
 ) 
