e7ef69203ad5e5e80feaa7efcf90d859a67b922e
NavodContrath <navod.contrath@gmail.com>
2025-02-19 15:32:58 +0100

    add milestone 2

--- main.js
+++ main.js
@@ -19,3 +19,32 @@ if (age > 65) {
 }
 */
 //MILESTONE 2
+const priceKm = 0.21
+const formEl = document.querySelector("form")
+const fullNameEl = document.getElementById("fullname")
+const userKmEl = document.getElementById("userKm")
+const userAgeEl = document.getElementById("userAge")
+
+
+formEl.addEventListener('submit', function () {
+    event.preventDefault()
+    let travelPrice = Number(userKmEl.value) * priceKm
+    if (Number(userAgeEl.value) > 65) {
+        const overDiscount = travelPrice * 40 / 100
+        console.log(travelPrice);
+        console.log(overDiscount);
+        const discountOver = travelPrice - overDiscount
+        console.log(discountOver.toFixed(2));
+    } else if (Number(userAgeEl.value) < 18) {
+        const minorDiscount = travelPrice * 20 / 100
+        console.log(travelPrice);
+        console.log(minorDiscount);
+        const discountMinor = travelPrice - minorDiscount
+        console.log(discountMinor.toFixed(2));
+    } else if (isNaN(userKmEl) || isNaN(userAgeEl)) {
+        alert("please insert a numeric value")
+    } else {
+        console.log(travelPrice);
+    }
+}
+)
